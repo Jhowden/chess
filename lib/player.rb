0a1855e294abf9671556cac3dd2841b5032e3c13
Jonathan Howden <jonathanhowden@gmail.com>
2014-05-29 23:52:12 -0500

    Added more functionality to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,17 @@
 class Player
 
-  attr_reader :team
+  attr_reader :team, :team_pieces, :king_piece
 
   def initialize( team )
     @team = team
   end
 
+  def set_team_pieces( pieces )
+    @team_pieces = []
+    team_pieces.concat pieces
+  end
+
+  def find_king_piece
+    @king_piece = team_pieces.detect { |piece| piece.is_a? King }
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,8 +3,25 @@ require "spec_helper"
 describe Player do 
 
   let( :player ) { described_class.new( :white ) }
+  let( :piece ) { double() }
+  let( :piece2 ) { double() }
+
+  before(:each) do
+    allow( piece2 ).to receive( :is_a? ).and_return true
+  end
 
   it "returns a players team" do
     expect( player.team ).to eq( :white )
+  end
+
+  it "sets the pieces for the player" do
+    player.set_team_pieces( [piece] )
+    expect( player.team_pieces.size ).to eq( 1 )
+  end
+
+  it "finds the player's king piece" do
+    player.set_team_pieces( [piece, piece2] )
+    player.find_king_piece
+    expect( player.king_piece ).to_not be_nil
   end
 end
