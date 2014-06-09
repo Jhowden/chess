d88e8a0d2bc64870322af6defa3788120aec9be4
Jonathan Howden <jhowden@jhowden.local>
2014-06-08 22:30:38 -0500

    Changed some tests

--- spec/checkmate_spec.rb
+++ spec/checkmate_spec.rb
@@ -21,7 +21,7 @@ describe Checkmate do
       expect( king ).to receive( :position ).exactly( 6 ).times
       expect( king ).to receive( :determine_possible_moves ).and_return( [["c", 4], ["d", 3], ["e", 3], ["e", 4], ["d", 5], ["c", 5]] )
       expect( game ).to receive( :move_piece! ).exactly( 6 ).times
-      expect( game ).to receive( :player_in_check? ).exactly( 6 ).times
+      expect( checkmate ).to receive( :check? ).exactly( 6 ).times
       expect( game ).to receive( :replace_board ).exactly( 6 ).times
       expect( piece ).to receive( :replace_board ).exactly( 6 ).times
       checkmate.move_king_in_all_possible_spots( player, player2 )
@@ -55,20 +55,17 @@ describe Checkmate do
   
   describe "#capture_pieces_threatening_king" do
     
-    before(:each) do
+    it "checks to see if good pieces can capture the enemy piece threatening the king" do
       allow( piece ).to receive( :position ).and_return piece
       allow( piece ).to receive( :file ).and_return "a"
       allow( piece ).to receive( :rank ).and_return 5
-      allow( game ).to receive( :convert_to_file_and_rank ).and_return [0, 3]
-      allow( game ).to receive( :player_in_check? ).and_return false
-    end
-    
-    it "checks to see if good pieces can capture the enemy piece threatening the king" do
       expect( game ).to receive( :board ).and_return game
       expect( game ).to receive( :dup ).and_return( Array.new( 8 ) { |cell| Array.new( 8 ) } )
       expect( player ).to receive( :team_pieces ).and_return( [piece2, piece3] )
       expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
         and_return( { piece => [["a", 4], ["b", 4], ["d", 5]] } )
+      expect( game ).to receive( :convert_to_file_and_rank ).and_return [0, 3]
+      expect( checkmate ).to receive( :check? ).and_return false
       expect( game ).to receive( :move_piece! )
       expect( piece2 ).to receive( :determine_possible_moves ).and_return( [["a", 5], ["a", 4], ["a", 3], ["a", 2], ["a", 1]] )
       expect( piece3 ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 3], ["c", 2], ["d", 1]] )
@@ -107,11 +104,11 @@ describe Checkmate do
       expect( checkmate.possible_moves ).to eq( [["a", 4], ["a", 3], ["a", 2], ["a", 4]] )
     end
     
-    # it "exits out of the method if there are more than one enemy piece that has the king in check" do
-    #   expect( player ).to_not receive( :team_pieces )
-    #   expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
-    #     and_return( { piece => [["a", 4], ["b", 4], ["d", 5]], piece3 => [["a", 4], ["b", 3], ["c", 2], ["d", 1]] }  )
-    #   checkmate.block_enemy_piece( player, player2 )
-    # end
+    it "exits out of the method if there are more than one enemy piece that has the king in check" do
+      expect( player ).to_not receive( :team_pieces )
+      expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
+        and_return( { piece => [["a", 4], ["b", 4], ["d", 5]], piece3 => [["a", 4], ["b", 3], ["c", 2], ["d", 1]] }  )
+      checkmate.block_enemy_piece( player, player2 )
+    end
   end
 end
