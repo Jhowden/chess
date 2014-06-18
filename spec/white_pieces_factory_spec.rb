68cb4af38c0c438226f84cc5076aacdd700db489
Jonathan Howden <jonathanhowden@gmail.com>
2014-06-17 23:00:27 -0500

    Modified pieces factory to reduce duplication, need to rename and delete black pieces factory

--- spec/white_pieces_factory_spec.rb
+++ spec/white_pieces_factory_spec.rb
@@ -3,74 +3,207 @@ require "spec_helper"
 describe WhitePiecesFactory do
 
   let(:board) { double() }
-  let(:white_pieces_factory) { described_class.new( board ) }
+  let(:white_pieces_factory) { described_class.new( board, :white ) }
 
   describe "#create_pawns" do
-    it "creates the white pawns" do
+    before( :each ) do
       white_pieces_factory.create_pawns
+      @first_pawn = white_pieces_factory.pieces.first
+    end
+
+    it "creates the 8 white pawns" do
       expect( white_pieces_factory.pieces.size ).to eq( 8 )
-      white_pieces_factory.pieces.each do |pawn|
-        expect( pawn ).to be_an_instance_of Pawn
-      end
+    end
+
+    it "creates a pawn with the correct board marker" do
+      expect( @first_pawn.board_marker ).to eq( "♙" )
+    end
+
+    it "creates a pawn with the correct file" do
+      expect( @first_pawn.position.file ).to eq( "a" )
+    end
+
+    it "creates a pawn with the correct rank" do
+      expect( @first_pawn.position.rank ).to eq( 7 )
+    end
+
+    it "creates a pawn with the correct team" do
+      expect( @first_pawn.team ).to eq( :white )
+    end
+
+    it "creates a pawn with the correct board" do
+      expect( @first_pawn.board ).to eq( board )
+    end
+
+    it "creates a pawn with the correct orientation" do
+      expect( @first_pawn.orientation ).to eq( :down )
     end
   end
 
   describe "#create_rooks" do
-    it "creates the white rooks" do
+
+    before( :each ) do
       white_pieces_factory.create_rooks
+      @first_rook = white_pieces_factory.pieces.first
+    end
+
+    it "creates the white rooks" do
       expect( white_pieces_factory.pieces.size ).to eq( 2 )
-      white_pieces_factory.pieces.each do |rook|
-        expect( rook ).to be_an_instance_of Rook
-      end
+    end
+
+    it "creates a rook with the correct board marker" do
+      expect( @first_rook.board_marker ).to eq( "♖" )
+    end
+
+    it "creates a rook with the correct file" do
+      expect( @first_rook.position.file ).to eq( "a" )
+    end
+
+    it "creates a rook with the correct rank" do
+      expect( @first_rook.position.rank ).to eq( 8 )
+    end
+
+    it "creates a rook with the correct team" do
+      expect( @first_rook.team ).to eq( :white )
+    end
+
+    it "creates a rook with the correct board" do
+      expect( @first_rook.board ).to eq( board )
     end
   end
 
   describe "#create_bishops" do
-    it "creates the white bishops" do
+
+    before( :each ) do
       white_pieces_factory.create_bishops
+      @first_bishop = white_pieces_factory.pieces.first
+    end
+
+    it "creates the white bishops" do
       expect( white_pieces_factory.pieces.size ).to eq( 2 )
-      white_pieces_factory.pieces.each do |bishop|
-        expect( bishop ).to be_an_instance_of Bishop
-      end
+    end
+
+    it "creates a bishop with the correct board marker" do
+      expect( @first_bishop.board_marker ).to eq( "♗" )
+    end
+
+    it "creates a bishop with the correct file" do
+      expect( @first_bishop.position.file ).to eq( "c" )
+    end
+
+    it "creates a bishop with the correct rank" do
+      expect( @first_bishop.position.rank ).to eq( 8 )
+    end
+
+    it "creates a bishop with the correct team" do
+      expect( @first_bishop.team ).to eq( :white )
+    end
+
+    it "creates a bishop with the correct board" do
+      expect( @first_bishop.board ).to eq( board )
     end
   end
 
   describe "#create_knights" do
-    it "creates the white knights" do
+
+    before( :each ) do
       white_pieces_factory.create_knights
+      @first_knight = white_pieces_factory.pieces.first
+    end
+
+    it "creates the white knights" do
       expect( white_pieces_factory.pieces.size ).to eq( 2 )
-      white_pieces_factory.pieces.each do |knight|
-        expect( knight ).to be_an_instance_of Knight
-      end
+    end
+
+    it "creates a knight with the correct board marker" do
+      expect( @first_knight.board_marker ).to eq( "♘" )
+    end
+
+    it "creates a knight with the correct file" do
+      expect( @first_knight.position.file ).to eq( "b" )
+    end
+
+    it "creates a knight with the correct rank" do
+      expect( @first_knight.position.rank ).to eq( 8 )
+    end
+
+    it "creates a knight with the correct team" do
+      expect( @first_knight.team ).to eq( :white )
+    end
+
+    it "creates a knight with the correct board" do
+      expect( @first_knight.board ).to eq( board )
     end
   end
 
   describe "#create_queen" do
-    it "creates the white queen" do
+
+    before( :each ) do
       white_pieces_factory.create_queen
+      @queen = white_pieces_factory.pieces.first
+    end
+
+    it "creates the white queen" do
       expect( white_pieces_factory.pieces.size ).to eq( 1 )
-      expect( white_pieces_factory.pieces.first ).to be_an_instance_of Queen
+    end
+
+    it "creates a queen with the correct board marker" do
+      expect( @queen.board_marker ).to eq( "♕" )
+    end
+
+    it "creates a queen with the correct file" do
+      expect( @queen.position.file ).to eq( "d" )
+    end
+
+    it "creates a queen with the correct rank" do
+      expect( @queen.position.rank ).to eq( 8 )
+    end
+
+    it "creates a queen with the correct team" do
+      expect( @queen.team ).to eq( :white )
+    end
+
+    it "creates a queen with the correct board" do
+      expect( @queen.board ).to eq( board )
     end
   end
 
   describe "#create_king" do
-    it "creates the white king queen" do
+
+    before( :each ) do
       white_pieces_factory.create_king
+      @king = white_pieces_factory.pieces.first
+    end
+
+    it "creates the white king queen" do
       expect( white_pieces_factory.pieces.size ).to eq( 1 )
-      expect( white_pieces_factory.pieces.first ).to be_an_instance_of King
+    end
+
+    it "creates a king with the correct board marker" do
+      expect( @king.board_marker ).to eq( "♔" )
+    end
+
+    it "creates a king with the correct file" do
+      expect( @king.position.file ).to eq( "e" )
+    end
+
+    it "creates a king with the correct rank" do
+      expect( @king.position.rank ).to eq( 8 )
+    end
+
+    it "creates a king with the correct team" do
+      expect( @king.team ).to eq( :white )
+    end
+
+    it "creates a king with the correct board" do
+      expect( @king.board ).to eq( board )
     end
   end
   
   describe "#build" do
     it "creates all of the white team's pieces" do
-      # Mocking or stubbing the thing you are testing is FORBIDDEN!
-      expect( white_pieces_factory ).to receive( :create_pawns )
-      expect( white_pieces_factory ).to receive( :create_rooks )
-      expect( white_pieces_factory ).to receive( :create_knights )
-      expect( white_pieces_factory ).to receive( :create_bishops )
-      expect( white_pieces_factory ).to receive( :create_queen )
-      expect( white_pieces_factory ).to receive( :create_king )
       white_pieces_factory.build
+      expect( white_pieces_factory.pieces.size ).to eq( 16 )
     end
   end
 end
