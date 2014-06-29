09c4d8af3ce0e46e27dbec08852f300252709f5d
Jonathan Howden <jonathanhowden@gmail.com>
2014-06-28 23:12:55 -0500

    Refactored class as well as updated tests

--- lib/checkmate.rb
+++ lib/checkmate.rb
@@ -10,39 +10,49 @@ class Checkmate
   def move_king_in_all_possible_spots( player, enemy_player )
     original_board = make_copy_of_original_board
     king = player.king_piece
-    possible_king_moves = king.determine_possible_moves
-    possible_king_moves.each do |possible_move|
+    king.determine_possible_moves.each do |possible_move|
       target_file, target_rank = game.convert_to_file_and_rank( possible_move.first, possible_move.last )
-      game.move_piece!( king, target_file, target_rank, king.position )
-      if !check?( player, enemy_player )
-        possible_moves << possible_move # pass in piece starting position and target position => ["a"3"b"4] so that I can ensure that when the player picks the king he chooses a correct spot
-      end
-      replace_board_on_pieces_to_original( [player, enemy_player], original_board )
-      # Having to remeber to put the board back to its original position is not good.  Use dupes and toss them away.
-      replace_board_on_game_to_original( original_board )
-      # 
+      game.update_the_board!( king, target_file, target_rank, king.position )
+      possible_moves << [king.position.file, king.position.rank].
+        concat( possible_move ) unless check?( player, enemy_player )
+      return_board_to_original_state( player, enemy_player, original_board)
     end
   end
   
   def capture_pieces_threatening_king( player, enemy_player )
-    enemy_pieces_threatening_king_collection = determine_enemy_piece_map( player, enemy_player ).keys
-    return if enemy_pieces_threatening_king_collection.size >= 2 # can't capture two enemy pieces in one move
+    enemy_pieces_collection = determine_enemy_piece_map( player, enemy_player ).keys
+    return if enemy_pieces_collection.size >= 2 # can't capture two enemy pieces in one move
     original_board = make_copy_of_original_board
     player.team_pieces.select{ |piece| !piece.captured? }.each do |piece|
-      piece_possible_moves = piece.determine_possible_moves
-      if piece_possible_moves.include?( [enemy_pieces_threatening_king_collection.first.position.file, 
-          enemy_pieces_threatening_king_collection.last.position.rank] )
-        target_file, target_rank = game.convert_to_file_and_rank( enemy_pieces_threatening_king_collection.first.position.file, 
-          enemy_pieces_threatening_king_collection.last.position.rank )
-        game.move_piece!( piece, target_file, target_rank, piece.position )
-        if !check?( player, enemy_player )
-          possible_moves << [enemy_pieces_threatening_king_collection.first.position.file, 
-            enemy_pieces_threatening_king_collection.last.position.rank] # pass in piece starting position and target position => ["a"3"b"4] so that I can ensure that when the player picks the piece he chooses a correct spot
-        end
-        replace_board_on_pieces_to_original( [player, enemy_player], original_board )
-        replace_board_on_game_to_original( original_board )
-      end
+      attempt_to_capture_enemy_piece( player, enemy_player, piece, enemy_pieces_collection, original_board )
     end
+  end
+
+  def attempt_to_capture_enemy_piece( player, enemy_player, piece, enemy_pieces_collection, original_board )
+    piece_possible_moves = piece.determine_possible_moves
+      if piece_possible_moves.include?( [enemy_pieces_collection.first.position.file, 
+          enemy_pieces_collection.last.position.rank] )
+        update_the_board!( piece, enemy_pieces_collection )
+        track_possible_moves( piece, enemy_pieces_collection ) unless check?( player, enemy_player )
+        return_board_to_original_state( player, enemy_player, original_board)
+      end
+  end
+
+  def update_the_board!( piece, enemy_pieces_collection )
+    target_file, target_rank = game.convert_to_file_and_rank( enemy_pieces_collection.first.position.file, 
+      enemy_pieces_collection.last.position.rank )
+    game.update_the_board!( piece, target_file, target_rank, piece.position )
+  end
+
+  def track_possible_moves( piece, enemy_pieces_collection )
+    possible_moves << [piece.position.file, piece.position.rank].
+      concat( [enemy_pieces_collection.first.position.file, 
+      enemy_pieces_collection.last.position.rank] )
+  end
+
+  def return_board_to_original_state( player, enemy_player, original_board)
+    replace_board_on_pieces_to_original( [player, enemy_player], original_board )
+    replace_board_on_game_to_original( original_board )
   end
   
   def block_enemy_piece( player, enemy_player )
@@ -55,12 +65,12 @@ class Checkmate
     	  if piece_possible_moves.include?( enemy_possible_move )
            target_file, target_rank = game.convert_to_file_and_rank( enemy_possible_move.first, 
               enemy_possible_move.last )
-            game.move_piece!( piece, target_file, target_rank, piece.position )
+            game.update_the_board!( piece, target_file, target_rank, piece.position )
           if !check?( player, enemy_player )
-    	      possible_moves << [enemy_possible_move.first, enemy_possible_move.last]
+    	      possible_moves << [piece.position.file, piece.position.rank].
+              concat( [enemy_possible_move.first, enemy_possible_move.last] )
           end
-          replace_board_on_pieces_to_original( [player, enemy_player], original_board )
-          replace_board_on_game_to_original( original_board )
+          return_board_to_original_state( player, enemy_player, original_board)
     		end
       end    
     end
--- spec/checkmate_spec.rb
+++ spec/checkmate_spec.rb
@@ -3,115 +3,120 @@ require_relative 'spec_helper'
 describe Checkmate do
   
   let(:game) { double() }
-  let(:checkmate) { described_class.new( game ) }
+  let(:board) { double() }
   let(:king) { double() }
   let(:piece) { double( captured?: false ) }
   let(:piece2) { double( captured?: false ) }
   let(:piece3) { double( captured?: false ) }
   let(:player) { double() }
   let(:player2) { double( team_pieces: [] ) }
+  let(:checkmate) { described_class.new( game ) }
+
+  before(:each) do
+    allow( player ).to receive( :team_pieces ).and_return [piece]
+    allow( king ).to receive( :position ).and_return king
+    allow( king ).to receive( :file ).and_return "b"
+    allow( king ).to receive( :rank ).and_return 4
+    allow( game ).to receive( :board ).and_return board
+    allow( game ).to receive( :replace_board )
+    allow( piece ).to receive( :replace_board )
+    allow( piece2 ).to receive( :replace_board )
+    allow( piece3 ).to receive( :replace_board )
+    allow( player ).to receive( :king_piece ).and_return king
+    allow( game ).to receive( :player_in_check? ).with( player, player2).
+      and_return false
+    allow( piece2 ).to receive( :position ).and_return piece2
+    allow( game ).to receive( :update_the_board! )
+  end
   
   describe "#move_king_in_all_possible_spots" do
+    before(:each) do
+      allow( king ).to receive( :determine_possible_moves ).
+        and_return( [["e", 6], ["f", 6], ["g", 6], ["e", 5], ["g", 5], ["e", 4], ["f", 4], ["g", 4]] )
+      allow( game ).to receive( :convert_to_file_and_rank ).
+        and_return( [[2, 4], [2, 5], [2, 6], [3, 4], [3, 6], [4, 4], [4,5], [4, 6]] )
+      allow( king ).to receive( :rank ).and_return 3
+    end
+
     it "checks to see where a king can move and not be in check" do
-      # Shouldn't some of these "expects" be "allows." How will the future programmer know what is important? 
-      allow( player ).to receive( :team_pieces ).and_return [piece]
-      expect( game ).to receive( :board ).and_return game
-      expect( game ).to receive( :dup ).and_return( Array.new( 8 ) { |cell| Array.new( 8 ) } )
-      expect( game ).to receive( :convert_to_file_and_rank ).exactly( 6 ).times.and_return( [[3, 4], [4, 3], [5, 3], [5, 4], [4, 5], [3, 5]] )
-      expect( player ).to receive( :king_piece ).and_return king
-      expect( king ).to receive( :position ).exactly( 6 ).times
-      expect( king ).to receive( :determine_possible_moves ).and_return( [["c", 4], ["d", 3], ["e", 3], ["e", 4], ["d", 5], ["c", 5]] )
-      expect( game ).to receive( :move_piece! ).exactly( 6 ).times
-      # Mocking or stubbing the thing you are testing is FORBIDDEN!
-      expect( checkmate ).to receive( :check? ).exactly( 6 ).times
-      expect( game ).to receive( :replace_board ).exactly( 6 ).times
-      expect( piece ).to receive( :replace_board ).exactly( 6 ).times
       checkmate.move_king_in_all_possible_spots( player, player2 )
-      expect( checkmate.possible_moves ).to eq( [["c", 4], ["d", 3], ["e", 3], ["e", 4], ["d", 5], ["c", 5]] )
+      expect( checkmate.possible_moves ).
+        to eq( [["b", 3, "e", 6], ["b", 3, "f", 6], ["b", 3, "g", 6], 
+                ["b", 3, "e", 5], ["b", 3, "g", 5], ["b", 3, "e", 4], 
+                ["b", 3, "f", 4], ["b", 3, "g", 4]] )
     end
   end
   
   describe "#determine_enemy_piece_map" do
+    before(:each) do
+      allow( piece ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 4], ["d", 5]] )
+      allow( player2 ).to receive( :king_piece ).and_return king
+    end
+
     it "creates a map of the piece's who can capture the king" do
-      allow( player ).to receive( :team_pieces ).and_return [piece]
-      expect( piece ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 4], ["d", 5]] )
-      expect( player2 ).to receive( :king_piece ).and_return king
-      expect( king ).to receive( :position ).twice.and_return king
-      expect( king ).to receive( :file ).and_return "b"
-      expect( king ).to receive( :rank ).and_return 4
       expect( checkmate.determine_enemy_piece_map( player2, player ) ).to eq( { piece => [["a", 4], ["b", 4], ["d", 5]] } )
     end
     
     it "can handle multiple pieces that put the king in check" do
       allow( player ).to receive( :team_pieces ).and_return [piece, piece2]
-      expect( piece ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 4], ["c", 5]] )
-      expect( piece2 ).to receive( :determine_possible_moves ).and_return( [["h", 4], ["b", 4], ["b", 4]] )
-      expect( player2 ).to receive( :king_piece ).and_return king
-      expect( king ).to receive( :position ).exactly( 4 ).times.and_return king
-      expect( king ).to receive( :file ).twice.and_return "b"
-      expect( king ).to receive( :rank ).twice.and_return 4
+      allow( piece2 ).to receive( :determine_possible_moves ).and_return( [["h", 4], ["b", 4], ["b", 4]] )
       expect( checkmate.determine_enemy_piece_map( player2, player ) ).
-        to eq( { piece => [["a", 4], ["b", 4], ["c", 5]], piece2 => [["h", 4], ["b", 4], ["b", 4]] } )
+        to eq( { piece => [["a", 4], ["b", 4], ["d", 5]], piece2 => [["h", 4], ["b", 4], ["b", 4]] } )
     end
   end
   
   describe "#capture_pieces_threatening_king" do
-    
     it "checks to see if good pieces can capture the enemy piece threatening the king" do
       allow( piece ).to receive( :position ).and_return piece
       allow( piece ).to receive( :file ).and_return "a"
       allow( piece ).to receive( :rank ).and_return 5
-      expect( game ).to receive( :board ).and_return game
-      expect( game ).to receive( :dup ).and_return( Array.new( 8 ) { |cell| Array.new( 8 ) } )
-      expect( player ).to receive( :team_pieces ).and_return( [piece2, piece3] )
-      # Mocking or stubbing the thing you are testing is FORBIDDEN!
-      expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
-        and_return( { piece => [["a", 4], ["b", 4], ["d", 5]] } )
-      expect( game ).to receive( :convert_to_file_and_rank ).and_return [0, 3]
-      expect( checkmate ).to receive( :check? ).and_return false
-      expect( game ).to receive( :move_piece! )
-      expect( piece2 ).to receive( :determine_possible_moves ).and_return( [["a", 5], ["a", 4], ["a", 3], ["a", 2], ["a", 1]] )
-      expect( piece3 ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 3], ["c", 2], ["d", 1]] )
-      expect( piece2 ).to receive( :position )
-      expect( checkmate ).to receive( :replace_board_on_pieces_to_original )
-      expect( checkmate ).to receive( :replace_board_on_game_to_original )
+      allow( player ).to receive( :team_pieces ).and_return( [piece2, piece3] )
+      allow( player2 ).to receive( :team_pieces ).and_return [piece]
+      allow( piece ).to receive( :determine_possible_moves ).
+        and_return( [["a", 4], ["b", 4], ["d", 5]])
+      allow( game ).to receive( :convert_to_file_and_rank ).and_return [0, 3]
+      allow( piece2 ).to receive( :determine_possible_moves ).
+        and_return( [["a", 5], ["a", 4], ["a", 3], ["a", 2], ["a", 1]] )
+      allow( piece3 ).to receive( :determine_possible_moves ).
+        and_return( [["a", 4], ["b", 3], ["c", 2], ["d", 1]] )
+      allow( piece2 ).to receive( :file ).and_return "d"
+      allow( piece2 ).to receive( :rank ).and_return 5
       checkmate.capture_pieces_threatening_king( player, player2 )
-      expect( checkmate.possible_moves ).to eq( [["a", 5]] )
+      expect( checkmate.possible_moves ).to eq( [["d", 5, "a", 5]] )
     end
     
     it "exits out of the method if there are more than one enemy piece that has the king in check" do
+      allow( player2 ).to receive( :team_pieces ).and_return [piece, piece3]
+      allow( piece ).to receive( :determine_possible_moves ).and_return [["b", 4], ["d", 5]]
+      allow( piece3 ).to receive( :determine_possible_moves ).and_return [["a", 4], ["b", 4]]
       expect( player ).to_not receive( :team_pieces )
-      expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
-        and_return( { piece => [["a", 4], ["b", 4], ["d", 5]], piece3 => [["a", 4], ["b", 3], ["c", 2], ["d", 1]] }  )
       checkmate.capture_pieces_threatening_king( player, player2 )
     end
   end
   
   describe "#block_enemy_piece" do
     it "checks to see if any good pieces can block the enemy piece threatening the king" do
-      expect( game ).to receive( :board ).and_return game
-      expect( game ).to receive( :dup ).and_return( Array.new( 8 ) { |cell| Array.new( 8 ) } )
-      expect( player ).to receive( :team_pieces ).and_return( [piece2, piece3] )
-      # Mocking or stubbing the thing you are testing is FORBIDDEN!
-      expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
-        and_return( { piece => [["a", 4], ["a", 3], ["a", 2], ["a", 1]] } )
-      expect( piece2 ).to receive( :determine_possible_moves ).and_return( [["a", 5], ["a", 4], ["a", 3], ["a", 2]] )
-      expect( piece3 ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 3], ["c", 2], ["d", 1]] )
-      expect( piece2 ).to receive( :position ).exactly( 3 ).times
-      expect( piece3 ).to receive( :position )
-      expect( game ).to receive( :move_piece! ).exactly( 4 ).times
-      expect( checkmate ).to receive( :replace_board_on_pieces_to_original ).exactly( 4 ).times
-      expect( checkmate ).to receive( :replace_board_on_game_to_original ).exactly( 4 ).times
-      expect( checkmate ).to receive( :check? ).exactly( 4 ).times.and_return false
-      expect( game ).to receive( :convert_to_file_and_rank ).exactly( 4 ).times.and_return [0, 3]
+      allow( player ).to receive( :team_pieces ).and_return( [piece2, piece3] )
+      allow( player2 ).to receive( :team_pieces ).and_return [piece]
+      allow( piece ).to receive( :determine_possible_moves ).
+        and_return( [["a", 4], ["a", 3], ["a", 2], ["a", 1], ["b", 4]] )
+      allow( game ).to receive( :convert_to_file_and_rank ).and_return [0, 3]
+      allow( piece2 ).to receive( :determine_possible_moves ).and_return( [["a", 5], ["a", 4], ["a", 3], ["a", 2]] )
+      allow( piece3 ).to receive( :determine_possible_moves ).and_return( [["a", 4], ["b", 3], ["c", 2], ["d", 1]] )
+      allow( piece2 ).to receive( :file ).and_return "d"
+      allow( piece2 ).to receive( :rank ).and_return 5
+      allow( piece3 ).to receive( :position ).and_return piece3
+      allow( piece3 ).to receive( :file ).and_return "c"
+      allow( piece3 ).to receive( :rank ).and_return 4
       checkmate.block_enemy_piece( player, player2 )
-      expect( checkmate.possible_moves ).to eq( [["a", 4], ["a", 3], ["a", 2], ["a", 4]] )
+      expect( checkmate.possible_moves ).to eq( [["d", 5, "a", 4], ["d", 5, "a", 3], ["d", 5, "a", 2], ["c", 4, "a", 4]] )
     end
     
     it "exits out of the method if there are more than one enemy piece that has the king in check" do
+      allow( player2 ).to receive( :team_pieces ).and_return [piece, piece3]
+      allow( piece ).to receive( :determine_possible_moves ).and_return [["b", 4], ["d", 5]]
+      allow( piece3 ).to receive( :determine_possible_moves ).and_return [["a", 4], ["b", 4]]
       expect( player ).to_not receive( :team_pieces )
-      expect( checkmate ).to receive( :determine_enemy_piece_map ).with( player, player2 ).
-        and_return( { piece => [["a", 4], ["b", 4], ["d", 5]], piece3 => [["a", 4], ["b", 3], ["c", 2], ["d", 1]] }  )
       checkmate.block_enemy_piece( player, player2 )
     end
   end
